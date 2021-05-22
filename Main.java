127570bc74903953ffb6b9e4d10173f13615bcd4
Yuri Goncharov <ygpanzer@gmail.com>
2021-05-22 13:59:17 +0200

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,23 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        PrintAny(1, 5);
+
+    }
+
+    public static void PrintAny(String some, String some2){
+
+        System.out.println("Line  " + some + " " + some2);
+
+    }
+
+    public static void PrintAny(int some, int some2){
+
+        System.out.println("Line  " + some + " " + some2);
+
+    }
+
+}
